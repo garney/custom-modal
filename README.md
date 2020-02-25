62d2b1a6561355de104f47c1052cc22cf793f6c1
Garney <garney.andres@3plearning.com>
2020-02-26 08:53:51 +1100

    initial commit and setup

---
+++ README.md
@@ -0,0 +1,26 @@
+custom-modal
+
+A simple reat custom modal
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```bash
+npm i custom-modal
+```
+
+## ✨ Usage
+
+```javascript
+
+```
+
+## 📌 Props
+
+Prop                  | Type     | Default                   | Required
+--------------------- | -------- | ------------------------- | --------
+`options`|any|`false`|No
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import EventDispatcher from '@garney/event-dispatcher';
+
+import './modal.scss';
+
+
+class ModalDispatcher extends EventDispatcher {
+  constructor() {
+    super();
+    this.state = false;
+  }
+
+  showModal(options) {
+    this.state = {
+      ...options,
+      visible: true
+    };
+    this.dispatchEvent(ModalDispatcher.events.SET_MODAL, this.state);
+  }
+
+  hideModal() {
+    this.state = {
+      visible: false
+    };
+    this.dispatchEvent(ModalDispatcher.events.SET_MODAL, this.state);
+  }
+  get status() {
+    return this.state;
+  }
+}
+
+ModalDispatcher.instance = new ModalDispatcher();
+ModalDispatcher.events = {
+  SET_MODAL: "SET_MODAL"
+};
+function Modal() {
+
+  const [options, setOptions] = useState({
+    visible: false,
+    customContent: null,
+    onClickClose
+  });
+
+  const modalStyle =  {
+    backgroundColor: 'rgba(0,0,0,.5)',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+
+  const popUpStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    background: '#fff',
+    padding: '5px 20px',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  const popUpContentStyle = {
+    display: 'flex',
+    minWidth: 200,
+    minHeight: 150,
+    alignItems: 'center',
+    justifyContent: 'center'
+  };
+
+  const popUpTitleStyle = {
+    marginTop: 5
+  };
+
+  useEffect(() => {
+    ModalDispatcher.instance.addEventListener(ModalDispatcher.events.SET_MODAL, (options) => {
+      setOptions(options);
+    })
+  }, []);
+
+  const title = options.title ? (
+      <h1 className="title" style={popUpTitleStyle}>
+        {options.title}
+      </h1>
+  ) : '';
+
+  const popUp = (
+      <div className="pop-up" style={popUpStyle}>
+        {title}
+        <div className="content" style={popUpContentStyle}>
+          {options.message}
+        </div>
+
+      </div>
+  );
+
+  const view = options.visible ? (
+      <div style={modalStyle} onClick={() =>{
+        ModalDispatcher.instance.hideModal();
+      }}>
+        {options.customContent ? options.customContent : popUp}
+      </div>
+  ): '';
+  return view;
+}
+
+function showModal(options) {
+  ModalDispatcher.instance.showModal(options)
+}
+
+function hideModal() {
+  ModalDispatcher.instance.hideModal()
+}
+
+
+module.exports = {
+  Modal,
+  showModal,
+  hideModal
+};
